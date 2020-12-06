455c1cebf1dc39a6d6d056089753b6818506296b
Blaon <iloveelevation@gmail.com>
2020-12-06 11:31:31 +0100

    SeasonsTablePage firstRecord Element and get text Method

--- src/test/java/pagesPOM/adminPages/SeasonsTablePage.java
+++ src/test/java/pagesPOM/adminPages/SeasonsTablePage.java
@@ -1,10 +1,19 @@
 package pagesPOM.adminPages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pagesPOM.CommonPageAttributes;
 
 public class SeasonsTablePage extends CommonPageAttributes {
     public SeasonsTablePage(WebDriver driver) {
         super(driver);
+    }
+
+@FindBy(xpath = "//tbody/tr[1]/td[2]/div")
+    WebElement firstRecordName;
+
+    public String getFirstRecordText(){
+        return firstRecordName.getText();
     }
 }
